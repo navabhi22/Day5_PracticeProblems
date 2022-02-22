02d3ed1f3d29e28a47ada04853c8f00da15cb0bd
navabhi22 <navabhi22@gmail.com>
2022-02-22 23:45:21 +0530

    Added Class to Calculate Power of Two

---
+++ PowerOfTwo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblems
+{
+    public class PowerOfTwo
+    {
+        /// <summary>
+        /// Calculates the power of two.
+        /// </summary>
+        /// <param name="N">The n.</param>
+        public static void CalculatePowerOfTwo(int N)
+        {
+            if (N < 31)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    double PowOf2 = Math.Pow(2, i);
+
+                    Console.WriteLine(PowOf2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter the value within 0 and 30");
+            }
+
+        }
+    }
+}
