f641c1dc5c67198d1bab7240e79a6c6853691f9e
navabhi22 <navabhi22@gmail.com>
2022-02-22 23:37:44 +0530

    Day5 Prcatice Problems

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day5_PracticeProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 6 Practice Problems ");
+            
+
+        }
+    }
+}
