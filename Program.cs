65cb9a478f3003fdd3072e44c5f7c8b27b8b4624
navabhi22 <navabhi22@gmail.com>
2022-02-22 23:54:26 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,9 +7,8 @@ namespace Day5_PracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Day 6 Practice Problems ");
-            Console.WriteLine("Enter the Power of Two");
-            int Power = Convert.ToInt32(Console.ReadLine());
-            PowerOfTwo.CalculatePowerOfTwo(Power);
+            
+            LeapYear.CheckLeapYear();
 
         }
     }
