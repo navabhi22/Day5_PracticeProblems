017cdf497c377367ffef9435e1b324ed9b6ecf79
navabhi22 <navabhi22@gmail.com>
2022-02-22 23:48:05 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace Day5_PracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Day 6 Practice Problems ");
-            
+            Console.WriteLine("Enter the value of N to get Nth number of Harmonic Series ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            HarmonicNumber.HarmonicNum(N);
 
         }
     }
