b2b04640c12f8725f692be9b63b17552d5cb611e
navabhi22 <navabhi22@gmail.com>
2022-02-22 23:40:41 +0530

    Added Class to Print Nth number of Harmonic Series

---
+++ HarmonicSeries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblems
+{
+    public class HarmonicNumber
+    {
+        /// <summary>
+        /// Nth number of Harmonic Series.
+        /// </summary>
+        /// <param name="N">The n.</param>
+        public static void HarmonicNum(int N)
+        {
+            double HarmonicSum = 0;
+            if (N > 0)
+            {
+                for (double i = 1; i <= N; i++)
+                {
+                    HarmonicSum = HarmonicSum + 1 / i;
+
+                }
+
+                Console.WriteLine("the Nth harmonic value is " + HarmonicSum);
+
+            }
+            else
+            {
+                Console.WriteLine(" N should be greter than 0 ");
+            }
+        }
+    }
+}
