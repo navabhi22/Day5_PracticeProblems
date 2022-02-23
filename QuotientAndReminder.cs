3838c64ee76c5efd33bd4247f56e63841ea57384
navabhi22 <navabhi22@gmail.com>
2022-02-23 10:18:34 +0530

    Added class to Calculate Quotient and Reminder

---
+++ QuotientAndReminder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblems
+{
+    public class QuotientAndReminder
+    {
+        public static void FindQuotientAndReminder(int dividend, int devisor)
+        {
+            
+            int quotient = dividend / devisor;
+            int reminder = dividend % devisor;
+
+            Console.WriteLine("Dividend : {0} Devisor {1}", dividend, devisor);
+            Console.WriteLine("Quotient " + quotient);
+            Console.WriteLine("Reminder " + reminder);
+        }
+    }
+}
