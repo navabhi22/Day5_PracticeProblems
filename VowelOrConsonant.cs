3de5ac13432e732c2c7d307a8eb83a075d046cae
navabhi22 <navabhi22@gmail.com>
2022-02-23 10:30:22 +0530

    Added class to Check Vowel or Consonant

---
+++ VowelOrConsonant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblems
+{
+    public class VowelOrConsonant
+    {
+        public static void CheckVowelAndConsonant()
+        {
+            Console.WriteLine("Enter a character");
+            char ch = Convert.ToChar(Console.ReadLine());
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine(ch + " is a Vowel");
+            }
+            else if ((ch >= 'a') && (ch <= 'z') || (ch >= 'A') && (ch <= 'Z'))
+            {
+                Console.WriteLine(ch + " is a Consonant");
+            }
+            else
+            {
+                Console.WriteLine("enter a valid character");
+            }
+        }
+    }
+}
