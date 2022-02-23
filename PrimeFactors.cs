250242e3e68cc509816bd967a261cb063d675a56
navabhi22 <navabhi22@gmail.com>
2022-02-23 10:03:38 +0530

    Added class to Find Prime Factors

---
+++ PrimeFactors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_PracticeProblems
+{
+    public class PrimeFactors
+    {
+        /// <summary>
+        /// Calculate the Primefactors.
+        /// </summary>
+        /// <param name="N">The n.</param>
+        public static void PrimeFactor(int N)
+        {
+
+            for (int i = 1; i <= N; i++)
+            {
+
+                int y = N % i;
+                if (y == 0 && PrimeNumber.PrimeNum(i) == true)
+                {
+
+                    Console.Write(i + "  ");
+
+                }
+
+            }
+
+        }
+    }
+}
