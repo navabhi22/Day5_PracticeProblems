3bf30ffd48f387f8cba6c3798da8e91425a2520e
navabhi22 <navabhi22@gmail.com>
2022-02-23 10:12:29 +0530

    Added class to Check Even Odd

---
+++ EvenOdd.cs
@@ -0,0 +1,26 @@
+﻿namespace Day5_PracticeProblems
+{
+    internal class EvenOdd
+    {
+        /// <summary>
+        /// Checks for Even and Odd number.
+        /// </summary>
+        /// <param name="N">The n.</param>
+        /// <returns></returns>
+        public static string CheckEvenOdd(int N)
+        {
+            if (N % 2 == 0)
+            {
+
+                return $"{N} is an even number";
+
+            }
+            else
+            {
+
+                return $"{N} is an Odd number";
+            }
+        }
+    }
+}
+
