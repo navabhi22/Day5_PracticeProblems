347305ade6e3930da3c9a5a1831011c71b2c450e
navabhi22 <navabhi22@gmail.com>
2022-02-23 10:30:33 +0530

    Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,13 +7,8 @@ namespace Day5_PracticeProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Day 6 Practice Problems ");
-            Console.WriteLine("Enter the number 1 ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the number 2 ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
-
-            SwapNumber.SwapNum(num1, num2);
-
+            VowelOrConsonant.CheckVowelAndConsonant();
+            
             
         }
     }
